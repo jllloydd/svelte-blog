f346a81db65d426a89099417f24016e1d1608274
jllloydd <wayfivet@gmail.com>
2025-01-29 21:53:22 +0800

    Added a floating action button for post creation.

--- src/routes/blog/components/BlogCard.svelte
+++ src/routes/blog/components/BlogCard.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="mb-3 flex flex-row items-center justify-between">
-	<h1 class="text-4xl font-bold text-white">All Blogs</h1>
-	<button
-		type="button"
-		class="text-dark rounded-lg bg-white px-2 py-2 text-center text-sm font-medium hover:bg-orange-300 focus:outline-none focus:ring-4 focus:ring-orange-300"
-		>Create your own post</button
-	>
-</div>
+<h1 class="mb-3 text-4xl font-bold text-white">All Blogs</h1>
 
 <article class="flex flex-col space-y-3 rounded-lg bg-white px-3 py-3 shadow-md">
 	<div class="flex items-center gap-3">
@@ -26,3 +19,22 @@
 	</div>
 	<!-- button probably leads to modal where all contents are seen -->
 </article>
+
+<!-- floating action button for creating posts -->
+<button
+	type="button"
+	class="fixed bottom-8 right-8 rounded-full bg-orange-700 p-3 font-bold text-white shadow-lg hover:bg-orange-800 focus:outline-none focus:ring-4 focus:ring-orange-300"
+>
+	<span class="flex items-center gap-2">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+		</svg>
+		<span>Create Post</span>
+	</span>
+</button>
